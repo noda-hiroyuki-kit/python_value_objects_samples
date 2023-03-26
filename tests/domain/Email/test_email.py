1d1731817489e9b6a1785d1ac31b83b383719450
noda-hiroyuki-kit <noda.hiroyuki@yk.commufa.jp>
2023-03-26 22:20:33 +0900

    test: 引数domain_partを追加
    コンストラクタは、引数domain_partも受けるようにテストを修正

--- tests/domain/Email/test_email.py
+++ tests/domain/Email/test_email.py
@@ -4,6 +4,8 @@ from value_objects_sample.domain.Email.email import Email
 class Test_Email:
     def test_コンストラクタはインスタンスを生成する(self):
         email = Email(
-            local_part='test'
+            local_part='test',
+            domain_part='tester.com'
         )
-        assert email._parts == 'test'
+        assert email._parts[0] == 'test'
+        assert email._parts[1] == 'tester.com'
