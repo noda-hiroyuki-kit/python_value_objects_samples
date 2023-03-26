6638dbb5eac04843c55ffda1ab5b19ab8ca58bec
noda-hiroyuki-kit <noda.hiroyuki@yk.commufa.jp>
2023-03-26 23:25:07 +0900

    test: test_コンストラクタは255文字超える長さメールアドレスはValueErrorを返すを追加
    メールアドレス255字超える長さのメールアドレスに対して、ValueErrorを出す。
    ValueErrorのメッセージは、"Email addresses too long"であること
    を確認するテストを追加

--- tests/domain/Email/test_email.py
+++ tests/domain/Email/test_email.py
@@ -1,3 +1,5 @@
+import pytest
+
 from value_objects_sample.domain.Email.email import Email
 
 
@@ -9,3 +11,11 @@ class Test_Email:
         )
         assert email._parts[0] == 'test'
         assert email._parts[1] == 'tester.com'
+
+    def test_コンストラクタは255文字超える長さメールアドレスはValueErrorを返す(self):
+        with pytest.raises(ValueError) as e:
+            Email(
+                local_part='test' * 61 + '1',
+                domain_part='tester.com'
+            )
+            assert str(e.value) == 'Email addresses too long' 
