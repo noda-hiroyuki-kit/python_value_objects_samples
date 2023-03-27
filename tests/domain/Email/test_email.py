b1bfeeed44b5e0cf7fb07edfa21b9c33aab36550
noda-hiroyuki-kit <noda.hiroyuki@yk.commufa.jp>
2023-03-28 04:34:38 +0900

    refactor: 文字列長255文字テストを追加とBug Fix
    メールアドレス文字列長255文字テストを追加したところ、テスト失敗
    コンストラクタに追加した255文字列長判定箇所にbugが判明し修正
    テストの文字列長も誤りがあったため修正

--- tests/domain/Email/test_email.py
+++ tests/domain/Email/test_email.py
@@ -12,10 +12,20 @@ class Test_Email:
         assert email._parts[0] == 'test'
         assert email._parts[1] == 'tester.com'
 
+    def test_コンストラクタは254文字長さメールアドレスはインスタンスを生成する(self):
+        test_local_part = 'test' * 61 + '1'
+        email = Email(
+            local_part=test_local_part,
+            domain_part='tester.com'
+        )
+        assert email._parts[0] == test_local_part
+        assert email._parts[1] == 'tester.com'
+
     def test_コンストラクタは255文字超える長さメールアドレスはValueErrorを返す(self):
         with pytest.raises(ValueError) as e:
             Email(
-                local_part='test' * 61 + '1',
+                local_part='test' * 61 + '12',
                 domain_part='tester.com'
             )
-            assert str(e.value) == 'Email addresses too long' 
+            assert str(e.value) == 'Email addresses too long'
+
--- value_objects_sample/domain/Email/email.py
+++ value_objects_sample/domain/Email/email.py
@@ -1,5 +1,5 @@
 class Email:
     def __init__(self, local_part, domain_part):
-        if len(local_part) + len(local_part) > 255:
+        if len(local_part) + len(domain_part) > 255:
             raise ValueError("Email addresses too long")
         self._parts = (local_part, domain_part)
