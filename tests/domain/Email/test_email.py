e50beb05dc4b7b8c9691f567a7858b5eb63b1177
noda-hiroyuki-kit <noda.hiroyuki@yk.commufa.jp>
2023-04-08 14:26:41 +0900

    refactor: テストコードを階層化
    テストコードをメソッド毎に階層化
    それに伴いテストのプロシージャ名を変更
    一部テストの不使用変数の削除

--- tests/domain/Email/test_email.py
+++ tests/domain/Email/test_email.py
@@ -4,56 +4,61 @@ from value_objects_sample.domain.Email.email import Email
 
 
 class Test_Email:
-    def test_from_textメソッドはインスタンスを生成する(self):
-        email = Email.from_text('test@tester.com')
-        assert email._parts[0] == 'test'
-        assert email._parts[1] == 'tester.com'
+    class Test_from_text名前の付いたコンストラクタ:
+        def test_インスタンスを生成する(self):
+            email = Email.from_text('test@tester.com')
+            assert email._parts[0] == 'test'
+            assert email._parts[1] == 'tester.com'
 
-    def test_from_textメソッドはアットマークがない引数の時はValueErrorを返す(self):
-        with pytest.raises(ValueError) as e:
-            email = Email.from_text('test_tester.com')
-            assert str(e.value) == "Email addresses must contains '@'"
-            
-    def test_コンストラクタはインスタンスを生成する(self):
-        email = Email(
-            local_part='test',
-            domain_part='tester.com'
-        )
-        assert email._parts[0] == 'test'
-        assert email._parts[1] == 'tester.com'
+        def test_アットマークがない引数の時はValueErrorを返す(self):
+            with pytest.raises(ValueError) as e:
+                Email.from_text('test_tester.com')
+                assert str(e.value) == "Email addresses must contains '@'"
 
-    def test_コンストラクタは254文字長さメールアドレスはインスタンスを生成する(self):
-        test_local_part = 'test' * 61 + '1'
-        email = Email(
-            local_part=test_local_part,
-            domain_part='tester.com'
-        )
-        assert email._parts[0] == test_local_part
-        assert email._parts[1] == 'tester.com'
-
-    def test_コンストラクタは255文字超える長さメールアドレスはValueErrorを返す(self):
-        with pytest.raises(ValueError) as e:
-            Email(
-                local_part='test' * 61 + '12',
+    class Test___init__コンストラクタ:
+        def test_インスタンスを生成する(self):
+            email = Email(
+                local_part='test',
                 domain_part='tester.com'
             )
-            assert str(e.value) == 'Email addresses too long'
+            assert email._parts[0] == 'test'
+            assert email._parts[1] == 'tester.com'
 
-    def test_strは登録したメールアドレスを返す(self):
-        email = Email.from_text('test@tester.com')
-        assert str(email) == 'test@tester.com'
+        def test_254文字長さメールアドレスはインスタンスを生成する(self):
+            test_local_part = 'test' * 61 + '1'
+            email = Email(
+                local_part=test_local_part,
+                domain_part='tester.com'
+            )
+            assert email._parts[0] == test_local_part
+            assert email._parts[1] == 'tester.com'
 
-    def test_reprは指定フォーマットの文字列を返す(self):
-        email = Email.from_text('test@tester.com')
-        assert email.__repr__() == "Email(local_part='test', domain_part='tester.com')"
+        def test_255文字超える長さメールアドレスはValueErrorを返す(self):
+            with pytest.raises(ValueError) as e:
+                Email(
+                    local_part='test' * 61 + '12',
+                    domain_part='tester.com'
+                )
+                assert str(e.value) == 'Email addresses too long'
 
-    def test_同じメールアドレスの場合は同じと評価する(self):
-        email1 = Email.from_text('test@tester.com')
-        email2 = Email.from_text('test@tester.com')
-        assert email1 == email2
+    class Test___str__メソッド:
+        def test_strは登録したメールアドレスを返す(self):
+            email = Email.from_text('test@tester.com')
+            assert str(email) == 'test@tester.com'
 
-    def test_Emailクラスとメールアドレスの比較はNotImplementedを返す(self):
-        email1 = Email.from_text('test@tester.com')
-        email2 = 'test@tester.com'
-        result = email1.__eq__(email2)
-        assert result == NotImplemented
+    class Test___repr__メソッド:
+        def test_reprは指定フォーマットの文字列を返す(self):
+            email = Email.from_text('test@tester.com')
+            assert email.__repr__() == "Email(local_part='test', domain_part='tester.com')"
+
+    class Test___eq__メソッド:
+        def test_Emailクラスの同じメールアドレスの場合は同じと評価する(self):
+            email1 = Email.from_text('test@tester.com')
+            email2 = Email.from_text('test@tester.com')
+            assert email1 == email2
+
+        def test_Emailクラスとメールアドレスの比較はNotImplementedを返す(self):
+            email1 = Email.from_text('test@tester.com')
+            email2 = 'test@tester.com'
+            result = email1.__eq__(email2)
+            assert result == NotImplemented
