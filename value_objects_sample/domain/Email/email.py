fbb6de123b5b2c157754807ee361673e63096029
noda-hiroyuki-kit <noda.hiroyuki@yk.commufa.jp>
2023-04-09 15:22:21 +0900

    refactor: 型定義を追加
    Emailクラスの各メソッドの引数、返す値に型を追加

--- value_objects_sample/domain/Email/email.py
+++ value_objects_sample/domain/Email/email.py
@@ -1,13 +1,16 @@
+from typing import Self
+
+
 class Email:
 
     @classmethod
-    def from_text(cls, address):
+    def from_text(cls, address: str) -> (Self | ValueError):
         if '@' not in address:
             raise ValueError("Email addresses must contains '@'")
         local_part, _, domain_part = address.partition('@')
         return cls(local_part, domain_part)
 
-    def __init__(self, local_part, domain_part):
+    def __init__(self, local_part: str, domain_part: str):
         if len(local_part) + len(domain_part) > 255:
             raise ValueError("Email addresses too long")
         self._parts = (local_part, domain_part)
@@ -18,25 +21,25 @@ class Email:
     def __repr__(self) -> str:
         return "Email(local_part={!r}, domain_part={!r})".format(*self._parts)
 
-    def __eq__(self, rhs) -> bool:
+    def __eq__(self, rhs: any) -> bool:
         if not isinstance(rhs, Email):
             return NotImplemented
         return self._parts == rhs._parts
 
-    def __ne__(self, rhs) -> bool:
+    def __ne__(self, rhs: any) -> bool:
         return not (self == rhs)
 
     def __hash__(self) -> str:
         return hash(self._parts)
 
     @property
-    def local(self):
+    def local(self) -> str:
         return self._parts[0]
 
     @property
-    def domain(self):
+    def domain(self) -> str:
         return self._parts[1]
 
-    def replace(self, local: str = None, domain: str = None):
+    def replace(self, local: str = None, domain: str = None) -> Self:
         return Email(local_part=local or self._parts[0],
                      domain_part=domain or self._parts[1])
